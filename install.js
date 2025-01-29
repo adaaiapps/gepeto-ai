39bc1b3caead4364ec516235156794705737d49b
angga riyansyah <angga.adaai@gmail.com>
2025-01-29 08:22:12 +0700

    perubahan install js

--- install.js
+++ install.js
@@ -1,30 +1,31 @@
 module.exports = {
-    run: [
-      {
-        method: "shell.run",
-        params: {
-          message: [
-            "git clone https://github.com/pinokiocomputer/gepeto gepeto_ai",
-          ],
-        },
+  run: [
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "if exist gepeto_ai (rmdir /s /q gepeto_ai)",
+          "git clone https://github.com/pinokiocomputer/gepeto gepeto_ai",
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        venv_python: "3.10",
+        path: "gepeto_ai",
+        message: [
+          "if exist env (rmdir /s /q env)",
+          "pip install -r requirements.txt",
+        ],
       },
-      {
-        method: "shell.run",
-        params: {
-          venv: "env",
-          venv_python: "3.10",
-          path: "gepeto_ai",
-          message: [
-            "pip install -r requirements.txt",
-          ],
-        },
+    },
+    {
+      method: "notify",
+      params: {
+        html: "Click the 'start' tab to get started!",
       },
-      {
-        method: "notify",
-        params: {
-          html: "Click the 'start' tab to get started!",
-        },
-      },
-    ],
-  };
-  
+    },
+  ],
+};
