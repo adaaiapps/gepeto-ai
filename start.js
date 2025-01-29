97964ee9a655f7af39884f0ec4c0a448acedcd9a
angga riyansyah <angga.adaai@gmail.com>
2025-01-29 17:23:27 +0700

    Perbarui start.js

--- start.js
+++ start.js
@@ -1,16 +1,30 @@
 module.exports = {
   daemon: true,
   run: [
+    // Membuat virtual environment jika belum ada
+    {
+      method: "shell.run",
+      params: {
+        path: "gepeto_ai",
+        message: [
+          "python -m venv env"
+        ],
+      },
+    },
+    // Mengaktifkan virtual environment dan menginstal dependensi
     {
       method: "shell.run",
       params: {
         venv: "env",
         path: "gepeto_ai",
         message: [
-          "uv pip install -r requirements.txt"
+          "pip install --upgrade pip", 
+          "pip install -r requirements.txt", 
+          "pip install python-dotenv"
         ],
       },
     },
+    // Menjalankan aplikasi setelah instalasi selesai
     {
       method: "shell.run",
       params: {
