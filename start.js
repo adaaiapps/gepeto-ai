c669f43054dd748e4c01b56f8c2d4780d20e4215
angga riyansyah <angga.adaai@gmail.com>
2025-01-29 17:13:41 +0700

    perubahan start.js

--- start.js
+++ start.js
@@ -7,7 +7,16 @@ module.exports = {
         venv: "env",
         path: "gepeto_ai",
         message: [
-          "uv pip install -r requirements.txt",
+          "uv pip install -r requirements.txt"
+        ],
+      },
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        path: "gepeto_ai",
+        message: [
           "python gepeto_ai.py"
         ],
       },
