84e67a9cf743b549879aadac638de6b3fb640219
angga riyansyah <angga.adaai@gmail.com>
2025-01-30 21:26:26 +0700

    perubahan start.js

--- pinokio.js
+++ pinokio.js
@@ -4,33 +4,6 @@ module.exports = {
   title: "gepeto",
   description: "Generate Pinokio Launchers, Instantly. https://gepeto.pinokio.computer",
   icon: "icon.jpeg",
-   pre: [{
-    title: "Git Repository URL",
-    description: "The URL of the Git repository to analyze",
-    env: "GIT_URL"
-  }, {
-    title: "API Key",
-    description: "The API key for the LLM",
-    env: "API_KEY"
-  }, {
-    title: "LLM Type",
-    description: "The type of LLM to use (openai, anthropic, google)",
-    env: "LLM_TYPE",
-    default: "openai"
-  }, {
-    title: "Pinokio Home",
-    description: "The Pinokio home directory",
-    env: "PINOKIO_HOME",
-    default: "/PINOKIO_HOME"
-  }, {
-    title: "Project Name",
-    description: "The name of the project",
-    env: "PROJECT_NAME"
-  }, {
-    title: "Icon URL",
-    description: "The URL of the icon",
-    env: "ICON_URL"
-  }],
   menu: async (kernel, info) => {
     let running = info.running("start.js")
     if (running) {
