4079eed9c37e8bc4f3561163f4edf450399ed086
angga riyansyah <angga.adaai@gmail.com>
2025-01-30 22:18:26 +0700

    perubahan start.js dan membuat file install.js

---
+++ install.js
@@ -0,0 +1,47 @@
+module.exports = {
+    run: [
+      // Edit this step to customize the git repository to use
+      {
+        method: "shell.run",
+        params: {
+          message: [
+            "git clone {{env.GIT_URL}} app",
+          ]
+        }
+      },
+      // Edit this step with your custom install commands
+      {
+        method: "shell.run",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          message: [
+            "pip install gradio devicetorch",
+            "pip install -r requirements.txt",
+            "playwright install --with-deps"
+          ]
+        }
+      },
+      {
+        method: "shell.run",
+        params: {
+          path: "app/ui",
+          message: "npm install"
+        }
+      },
+      //  Uncomment this step to add automatic venv deduplication (Experimental)
+      //  {
+      //    method: "fs.link",
+      //    params: {
+      //      venv: "env"
+      //    }
+      //  },
+      {
+        method: "notify",
+        params: {
+          html: "Click the 'start' tab to get started!"
+        }
+      }
+    ]
+  }
+  
--- start.js
+++ start.js
@@ -3,64 +3,35 @@ const path = require('path');
 module.exports = {
   daemon: true,
   run: [
-    // Langkah 1: Membuat virtual environment jika belum ada
+    // Langkah 1: Jalankan backend
     {
       method: "shell.run",
       params: {
         message: [
-          "python -m venv env"
-        ],
-      },
-    },
-    // Langkah 2: Mengaktifkan virtual environment
-    {
-      method: "shell.run",
-      params: {
-        message: [
-          "{{platform === 'win32' ? 'call env\\\\Scripts\\\\activate' : 'source env/bin/activate'}}"
-        ],
-      },
-    },
-    // Langkah 3: Clone repository GitHub
-    {
-      method: "shell.run",
-      params: {
-        message: [
-          `git clone {{env.GIT_URL}} app`
-        ]
-      }
-    },
-    // Langkah 4: Instal dependencies
-    {
-      method: "shell.run",
-      params: {
-        message: [
-          "python -m pip install --upgrade pip",
-          "{{platform === 'win32' ? 'python -m pip install -r requirements.txt' : 'python3 -m pip install -r requirements.txt'}}"
-        ],
-        path: "app"
-      }
-    },
-    // Langkah 5: Jalankan aplikasi
-    {
-      method: "shell.run",
-      params: {
-        message: [
-          "python app.py"
+          "python {{env.PROJECT_NAME ? env.PROJECT_NAME : 'app'}}.py",
         ],
         path: "app",
         on: [{
-          "event": "/http:\/\/\\S+/",
+          "event": "/Gepeto is up and running/i",
           "done": true
         }]
       }
     },
-    // Langkah 6: Setel variabel lokal 'url'
+    // Langkah 2: Jalankan frontend
+    {
+      method: "shell.run",
+      params: {
+        path: "app/ui",
+        message: "npm run start",
+        on: [{ "event": "/http:\/\/\\S+/", "done": true }]
+      }
+    },
+    // Langkah 3: Setel variabel lokal 'url'
     {
       method: "local.set",
       params: {
         url: "{{input.event[0]}}"
       }
-    }
-  ]
+    },
+  ],
 };
