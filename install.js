59094d83899af339e9119174e2efde118a14a711
angga riyansyah <angga.adaai@gmail.com>
2025-01-31 00:54:45 +0700

    perubahan pada install.js

--- install.js
+++ install.js
@@ -7,7 +7,8 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install -r requirements.txt"
+          "cd app",
+          "uv pip install -r requirements.txt"
         ]
       }
     },
