e6c52528c32042697d4d2df5d72e329724eb3a6a
angga riyansyah <angga.adaai@gmail.com>
2025-01-30 18:12:57 +0700

    Menambahkan perubahan pada semua file

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
-// index.js
 const { exec } = require('child_process');
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const kleur = require('kleur');
 
 const main = async () => {
     // No command line arguments needed anymore
-    // const argv = yargs(hideBin(process.argv)).argv;
-    // const name = argv.name;
-    // const git = argv.git;
-    // const icon = argv.icon;
-    // const api_key = argv.api_key;
-    // const llm_type = argv.llm_type;
-
-    // if (!name) {
-    //     console.error(kleur.red("Project name is required. Use --name <project_name>"));
-    //     process.exit(1);
-    // }
-
-    // const command = `python gepeto_ai.py --name ${name} --git ${git} --icon ${icon} --api_key ${api_key} --llm_type ${llm_type}`;
-    // exec(command, (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.error(kleur.red(`Error executing command: ${error.message}`));
-    //         return;
-    //     }
-    //     if (stderr) {
-    //         console.error(kleur.yellow(`Stderr: ${stderr}`));
-    //     }
-    //     console.log(kleur.green(stdout));
-    // });
     // No longer needed, since the python script is called from start.js
-    console.log(kleur.green("index.js is no longer used to run the python script."));
+    console.log("index.js is no longer used to run the python script.");
 };
 
 main();
